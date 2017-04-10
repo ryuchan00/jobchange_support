e53e352b906e84c6d68279cb401dc4e6f49c9edb
yamakawa00 <leokun0210@gmail.com>
2017-04-10 09:06:59 +0900

    output javascript part

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function () {
+    //一定時間後に処理を行う（0.8秒後）
+    setTimeout(function () {
+        //idが"notice"のdiv要素をゆっくりとフェードアウト
+        $('.alert').fadeOut("slow");
+    }, 800);
+});
