c5d6881e1d55b08a30ff16b50285f57f1d161468
Ubuntu <ubuntu@rails-dev-box>
2017-06-21 15:34:15 +0000

    updat

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -5,12 +5,62 @@ class ToppagesController < ApplicationController
     # @code.code = '<?php echo "Hello, Woeld;"'
     @code.code = <<'EOS'
 <?php 
-echo 'ようこそ！プログラミングの世界へ';
+echo "Hello,World";
 EOS
   end
 
   def enter
-    @code = CodeForm.new(params[:code_form])
-    p @code.code
+    @code = CodeForm.new
+    code = params[:code_form][:code]
+
+    http = setup_http
+    res = Net::HTTP.post_form(URI.parse('http://api.paiza.io:80/runners/create'),
+                              {'source_code' => code,
+                               'language' => "php",
+                               'longpoll' => "true",
+                               'api_key' => "guest"
+                              })
+    puts res.body
+    # query = {source_code: code, language: "php", longpoll: "true", api_key: "guest"}.to_query
+    query = {
+        source_code: code,
+        language: "php",
+        longpoll: "true",
+        api_key: "guest"
+    }.to_query
+    p code
+    p query
+    res = http.post(
+        '/runners/create',
+        query
+    )
+    if res.code != '200'
+      #todo:リターンコードを返す処理を入れる
+      p "return false"
+      # exit()
+    end
+    json = JSON.parse(res.body)
+    id = json['id']
+
+    query = {id: id, api_key: "guest"}.to_query
+    res = http.get(
+        '/runners/get_details',
+        query
+    )
+    if res.code != '200'
+      #todo:リターンコードを返す処理を入れる
+      # exit()
+    end
+    json = JSON.parse(res.body)
+    p json['stdout']
+  end
+
+  private
+
+  #httpインスタンスを生成する。
+  def setup_http
+    http = Net::HTTP.new('http://api.paiza.io', 80)
+    http.use_ssl = true
+    return http
   end
 end
---
+++ config/initializers/stdlibs.rb
@@ -0,0 +1,6 @@
+require 'net/https'
+require 'uri'
+require 'json'
+require 'time'
+# HashからURLのクエリストリングを生成する。
+require 'active_support/core_ext'
