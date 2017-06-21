275ce2e2f2f4a76a9e9b09d0dbd4ae76c903edcd
yamakawa00 <leokun0210@gmail.com>
2017-06-21 00:36:19 +0000

    modify string

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,11 @@ class ToppagesController < ApplicationController
   def index
     @items = Item.order('updated_at DESC')
     @code = CodeForm.new
-    @code.code = '<?php echo "Hello, Woeld";'
+    # @code.code = '<?php echo "Hello, Woeld;"'
+    @code.code = <<'EOS'
+<?php 
+echo 'ようこそ！プログラミングの世界へ';
+EOS
   end
 
   def enter
